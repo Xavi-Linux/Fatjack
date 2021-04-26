0ce52c2bbe50e94afcd24a97f541e7dad9880408
Xavi Mañá <xavier.manya@gmail.com>
2021-04-26 13:23:29 +0200

    Interface development -interactivity

---
+++ interfaces/web/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(e){
+    
+    document.getElementById("play").addEventListener("click", function(e){
+        document.getElementById("player_info").style.visibility = "visible";
+    });
+});
