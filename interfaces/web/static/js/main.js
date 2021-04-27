ff53a40fd8b6e29da58888ccf8cc2bd8179f320d
Xavi Mañá <xavier.manya@gmail.com>
2021-04-27 12:52:38 +0200

    interface development - interactivity

--- interfaces/web/static/js/main.js
+++ interfaces/web/static/js/main.js
@@ -1,6 +1,27 @@
 document.addEventListener("DOMContentLoaded",function(e){
     document.forms["player_info"].reset();
 
+    function currencyConverter(value){
+        const options = {style: "currency", currency:"EUR"};
+        const formatter = new Intl.NumberFormat("en-US", options);
+        return formatter.format(value);
+    }
+
+    function convertCurrency(value){
+        return Number(value.replace(/[^0-9.-]+/g,""));
+    }
+
+    function toggleClass(element, clas, time){
+        function addClass(element, clas){
+            element.classList.add(clas);
+        }
+        function removeClass(element, clas){
+            element.classList.remove(clas);
+        }
+        addClass(element, clas);
+        setTimeout(function(){removeClass(element, clas)}, time);
+    }
+
     document.getElementById("play").addEventListener("click", function(e){
         document.getElementById("player_info").style.visibility = "visible";
     });
@@ -50,15 +71,45 @@ document.addEventListener("DOMContentLoaded",function(e){
         this.childNodes.forEach(function(element){
             element.disabled = true;
         });
-        
+
+        var cash_element = document.getElementById("total_cash");
+        cash_element.value = currencyConverter(cash);
+        toggleClass(cash_element, "high_input_text", 2000);
+
+        document.getElementById("bet_container").style.display = "block";
+        toggleClass(document.getElementById("bet_container"), "high_border", 2000);
+
         e.preventDefault();
     });
 
-    function disable_instructors(){
+    document.getElementById("bet_placer").addEventListener("click", function(e){
+        var bet = document.getElementById("bet_value").value;
+        var remaining_cash = document.getElementById("total_cash").value;
+        var money = convertCurrency(remaining_cash);
+        var last_error = document.querySelector(".err");
+        
+        if (bet < 0 || bet > money || bet == ""){
+            if (last_error == null){
+                var text_error = "<p class='err'>Error!Bet value must be between €1 and " + remaining_cash + "!</p>";
+                var error_element = document.createElement("p");
+                error_element.innerHTML = text_error;
+                this.parentNode.append(error_element);
+            }            
+        }else {
+            if (last_error != null){
+                last_error.parentNode.removeChild(last_error);            
+            }
+        }
+    });
+
+    function resetAll(){
         document.querySelectorAll(".instruction").forEach(function(element){
             element.disabled = true;
         });
+        document.getElementById("total_cash").value = "€0.00";
+        document.getElementById("current_bet").value = "€0.00";
+        document.getElementById("bet_value").value = "";
     }
 
-    disable_instructors();
+    resetAll();
 });
